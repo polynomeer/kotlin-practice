b673601c50141e756143cd4db32d5582828959ba
polynomeer <polynomeer@gmail.com>
2025-03-11 22:04:18 +0900

    feat: Add sample for inheritance

---
+++ kotlin-basic/src/Parent.kt
@@ -0,0 +1,43 @@
+open class Parent(val familyName: String)
+
+/*class Child : Parent {
+    private val subName: String
+
+    constructor(subName: String) : this(subName, "")
+
+    constructor(subName: String, familyName: String) : super(familyName) {
+        this.subName = subName
+    }
+}*/
+
+class Child(
+    private val subName: String,
+    familyName: String
+) : Parent(familyName)
+
+class Account {
+    var balance: Long = 0
+        set(value) {
+            if (value < 0) throw IllegalArgumentException("음수 잔액은 불가능합니다.")
+            field = value
+        }
+
+    var accountName: String = ""
+        get() = "계좌이름:$field"
+}
+
+fun main() {
+    val child = Child("benny", "ham")
+    println("familyName is ${child.familyName}")
+
+    val account = Account()
+    account.balance = 100
+    account.accountName = "급여계좌"
+
+    println("balance : ${account.balance}, name: ${account.accountName}")
+
+    account.balance = -100
+    account.accountName = "급여계좌(수정)"
+
+    println("balance : ${account.balance}, name: ${account.accountName}")
+}
